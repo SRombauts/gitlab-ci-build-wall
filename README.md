cfbb395a3c21405747926b4a927ac13c8bfd91d6
Sébastien Rombauts <sebastien.rombauts@cofelyineo-gdfsuez.com>
2016-12-14 13:18:52 +0100

    Convert to Gitlab CI Pipeline and Build event Webhooks
    Keep previous build status to display as background while pending/running

--- README.md
+++ README.md
@@ -1,43 +1,9 @@
-jenkins-branch-wall
-===================
+gitlab-ci-build-wall
+====================
 
-A WEB page displaying status of feature branches - for GIT and maybe more ...
-
-Jenkins is a really wonderful tool, but is really cr**py when it comes to feature branches.
-
-I've been using Jenkins with GIT for few years now.
-It's really powerful handling 'master' and 'next' : you can have a clear view of the status of theses branches. When it comes to features branches, specially when you have plenty, you can't have a clear view of the status of each branch.
-
-Some plugins exists but none of them were satisfying my needs. Most of them use a template to create one job per branch. At the end you have as many jobs as feature branches. Good solution if you have few features, in the other case your continuous integration looks like yellow pages.
-
-Some example of plugins:
-- [Jenkins build per branch](http://entagen.github.io/jenkins-build-per-branch/)
-- [Jenkins autojobs](http://gvalkov.github.io/jenkins-autojobs/)
-
-
-A simple solution is to configure a special job for features branches.
-Let's assume your repository have this branch naming convention :
-- master : current,
-- next : preparation of the next release,
-- feature/xxxxxx : development of a new features,
-- ticket/xxxxx : correction of bugs submitted via your bugtracking portal.
-
-It's easy to configure a "branches" jobs that will run on every branch except master and next (these should be tests by dedicated jobs). To achieve this you just have to specify "origin/*/*" in the "Branches to build" / "Branch Specifier (blank for 'any')" in the configuration of the job.
-
-You have a job that runs on every branch ! Pretty cool. But ... (there's always a but after a cool) the main view of jenkins only shows the last results. It means that if the jobs ran on 3 different branches the main view will only show the result of the last run. The two preceding won't appear on the view. Of course you can ask jenkins to send emails, tweets, irc, ... but you will never get a simple and clear view of the state of your branches witch is really essential to me and my colleagues. Having a simple and clear view just in front of your eyes is a key point to be reactive.
-
-So I need a solution that show the result of each (feature) branch on a single page.
-I wrote a little script that keep a trace of each build result and is able to build an HTML table from these. This is simple an efficient. As the script build an HTML table, it was obvious that it should be hosted on a web server.
-Using the [notification plugin](https://wiki.jenkins-ci.org/display/JENKINS/Notification+Plugin), Jenkins is able to notify the build status of each job by sending a short summary of the build to an URL. In this summary, jenkins send the build result (failed / success), the SCM branch (yeessss !), a link to the build result page and others stuffs.
-
+A WEB page displaying status of feature branches of Gitlab CI
 
 To use this script :
-- deploy this script on a http server with python installed on it,
-- add the Job Notifications plugin to Jenkins
-- configure the job with a notification endpoint :
--- Format		 JSON
--- Protocol	 HTTP
--- Event		 Job finalized
--- URL       <the URL of your web server>/branches_status.py?variant=status
--- Timeout (in ms)	What you thing is ok (3000ms should be sufficient)
+- deploy this script on a http server (Apache...) with python installed on it and cgi activated for *.py files
+- configure Gitlab Webhooks for Pipeline and Build events
 
--- branches_status.py
+++ branches_status.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import cgi			# CGI
-import cgitb		# CGI- info de debug en cas de plantage
+import cgitb		# CGI - debug exception handler
 import urlparse
-import urllib
-import urllib2
-import datetime
+from datetime import datetime
 import os
 import sys
 import json
 import PyJSONSerialization
+import traceback
 
+SCRIPT_VERSION      = "v1.3"
 BRANCHES_STATUS     = "branches_status.json" # JSON status of branches
-MAX_BRANCH_PER_PAGE = 10
+MAX_BRANCH_PER_PAGE = 11
 
 CSS = '''
 html { 
@@ -28,7 +28,7 @@ html {
 }
 
 #titre {
-  font-size: 4em;
+  font-size: 3.5em;
   font-family: "Arial", Arial, sans-serif;
   text-align: center;
 }
@@ -41,32 +41,58 @@ html {
 }
 
 table {
-  margin: 10px 0 30px 0;
+  margin: 5px 0 30px 0;
 }
 
 table tr th, table tr td {
   background: #554444;
   color: #FFF;
-  border-radius: 10px;
-  padding: 7px 4px;
+  border-radius: 8px;
+  padding: 8px 2px;
   font-family: "Arial", Arial, sans-serif;
-  font-size: 2em;
-  text-align: left;
 }
 
 table tr th {
   text-align: center;
+  font-size: 1.6em;
 }
   
 table tr td {
   background: #444444;
+  font-size: 1.4em;
 }
 
-table .branche {
+table .branch {
   font-size: 3em;
 }
 
-table .SUCCESS { 
+table .pipeline { 
+  background: #444444;
+  text-align: center;
+}
+
+table .pending { 
+  background: #000000;
+  text-align: center;
+  padding: 4px 4px;
+  border-radius: 6px;
+}
+
+table .created { 
+  background: #000000;
+  text-align: center;
+  padding: 4px 4px;
+  border-radius: 6px;
+}
+
+table .running { 
+  background: #444444;
+  text-align: center;
+  padding: 4px 4px;
+  border-radius: 6px;
+}
+
+table .success { 
   background: #00BB00;
   text-align: center;
 }
@@ -76,14 +102,8 @@ table .OK {
   text-align: center;
 }
 
-table .FAILURE {
-  background: #BB0000;
-  text-align: center;
-}
-
-table .UNSTABLE {
-  background: #F6FF00;
-  color: #000000;
+table .failed {
+  background: #DD0000;
   text-align: center;
 }
 
@@ -91,8 +111,9 @@ a {
   text-decoration: none;
 }'''
 
-# Enable exception formating to HTML
+# activates a special exception handler that will display detailed reports in the Web browser if any errors occur
 cgitb.enable()
+sys.stderr = sys.stdout
 
 def escape(txt):
 	return cgi.escape(txt, True)
@@ -101,25 +122,57 @@ def escape(txt):
 # 1 variant = 1 column shown on the display
 class VariantStatus:
 	def __init__(self):
-		self.status = None
-		self.url    = None
+		self.build_id  = None
+		self.status    = None # 'pending' 'created' 'skipped' 'running' 'success' 'failed' 'canceled' 'OK'
+		self.previous  = None # 'success' 'failed' 'OK'
+		self.url       = None
 
 	@classmethod
-	def create(cls, status, url):
+	def create(cls, status, previous, url, build_id):
 		retour = cls()
-		retour.status = status
-		retour.url    = url
+		retour.build_id   = build_id
+		retour.status     = status
+		retour.previous   = previous
+		retour.url        = url
 		return retour
 
 # Status of a branch
 class BranchStatus:
 	def __init__(self):
-		self.date_maj = None
-		self.variants = dict()
-		
-	def set_result (self, variant, status, url):
-		self.variants[variant] = VariantStatus.create(status, url)
-		self.date_maj = datetime.datetime.now().isoformat()
+		self.pipeline_id = 0
+		self.url         = None
+		self.variants    = dict()
+		self.date_maj    = None
+
+	def set_id(self, pipeline_id, url):
+		self.pipeline_id = pipeline_id
+		self.url         = url
+
+	def set_result (self, variant, status, url, build_id):
+		old_build_id = 0
+		previous = None
+		if self.variants.has_key(variant):
+			old_build_id = self.variants[variant].build_id
+			old_status   = self.variants[variant].status   # 'pending' 'created' 'skipped' 'running' 'success' 'failed' 'canceled' 'OK'
+			old_previous = self.variants[variant].previous # 'success' 'failed' 'OK'
+			# Update "previous" build status during a new build ("pending", "created", "running" or "canceled") if it was relevant ("success", "failed" or "OK")
+			if (status == "pending" or status == "created" or status == "running" or status == "canceled") and (old_status == "success" or old_status == "failed" or old_status == "OK"):
+				previous = self.variants[variant].status
+			else: # Else, by default keep "previous" build status if any
+				previous = old_previous
+		# Only keep information on the most recent build for the variant
+		if build_id >= old_build_id:
+			self.variants[variant] = VariantStatus.create(status, previous, url, build_id)
+			self.date_maj = datetime.now().isoformat()
+
+	def force_result (self, variant, status):
+		if self.variants.has_key(variant):
+			url      = self.variants[variant].url
+			build_id = self.variants[variant].build_id
+		else:
+			url      = None
+			build_id = 0
+		set_result(variant, status, url, build_id)
 
 
 #########################################################################################################################
@@ -127,80 +180,113 @@ class BranchStatus:
 #          Main
 
 
-status = None
-branch = None
+save_to_file = False
 
-# Read parameters passed by the command line (CGI)
-get_params = urlparse.parse_qs(os.environ['QUERY_STRING'])
-try:
-	variant  = get_params['variant'][0]
-except:
-	variant = "Status"
-
-try:
-	MAX_BRANCH_PER_PAGE = int(get_params['view_size'][0])
-except:
-	pass
-	
-try:
-	force_status  = get_params['force_status'][0]
-	force_branch  = get_params['branch'][0]
-except:
-	force_status = None
-	force_branch = None
-
-# Read (if available) json data in the body of the request
-try:
-	myjson  = json.load(sys.stdin);
-	status  = str(myjson["build"]["status"])
-	branch = str(myjson["build"]["scm"]["branch"])
-	url     = str(myjson["build"]["full_url"])
-except:
-	# no data, this is not a jenkins request
-	pass
-
-
-# Chargement des données à partir du fichier
+# Load previous CI results from file
 try:
 	with open(BRANCHES_STATUS, "r") as f:
 		branch_list = PyJSONSerialization.load(f.read(), globals())
 except:
 	branch_list = dict()
 
-# Mise à jour des informations si elles sont fournies
-# (invoqué par Jenkins)
-if branch and variant and status :
-	if branch not in branch_list:
-		branch_list[branch] = BranchStatus()
-		
-	branch_list[branch].set_result(variant, status, url)
+# Read parameters passed by the command line (CGI)
+try:
+	get_params = urlparse.parse_qs(os.environ['QUERY_STRING'])
+	force_branch  = get_params['branch'][0]
+	force_variant = get_params['variant'][0]
+	force_status  = get_params['force_status'][0]
+	if force_branch and force_variant and force_status:
+		branch_list[force_branch].force_result(force_variant, force_status)
+		save_to_file = True
+except:
+	pass
 
-	# Sauvegarde le résultat
-	with open(BRANCHES_STATUS, "w") as f:
-		f.write (PyJSONSerialization.dump(branch_list))
+# Read json data (if available) in the body of the request
+try:
+  #json_status = json.load(sys.stdin)
+	raw_data = sys.stdin.read()
 
-if force_branch and variant and force_status :
-	branch_list[force_branch].set_result(variant, force_status, None)
-	
-	# Sauvegarde le résultat
-	with open(BRANCHES_STATUS, "w") as f:
-		f.write (PyJSONSerialization.dump(branch_list))
+	json_status = json.loads(raw_data)
 
-# Présente le résultat
-print '''Content-type: text/html;charset=utf-8'
+	if json_status["object_kind"] == "pipeline":
+		pipeline_id = json_status["object_attributes"]["id"]
+		branch      = json_status["object_attributes"]["ref"]
+		status      = json_status["object_attributes"]["status"] # 'pending' 'running' 'success' 'failed' 'canceled'
+		builds      = json_status["builds"]
+		web_url     = json_status["project"]["web_url"]
+
+		# Update CI results only if there is a new result provided by the Gitlab CI Pipeline Webhook
+		if branch not in branch_list:
+			update = True
+			branch_list[branch] = BranchStatus()
+		elif pipeline_id == branch_list[branch].pipeline_id:
+			update = True
+		elif pipeline_id > branch_list[branch].pipeline_id:
+			update = True
+		else:
+			update = False
+
+		if update:
+			url = web_url + "/pipelines/" + str(pipeline_id)  
+			branch_list[branch].set_id(pipeline_id, url)
+			save_to_file = True
+
+			for build in builds:
+				variant   = build["name"]
+				status    = build["status"]
+				build_id  = build["id"]
+				url       = web_url + "/builds/" + str(build_id)  
+				branch_list[branch].set_result(variant, status, url, build_id)
+
+	elif json_status["object_kind"] == "build":
+		branch   = json_status["ref"]
+		variant  = json_status["build_name"]
+		build_id = json_status["build_id"]
+		status   = json_status["build_status"]
+		web_url  = json_status["repository"]["homepage"]
+		url      = web_url + "/builds/" + str(build_id)  
+
+		if branch in branch_list:
+			if variant in branch_list[branch].variants:
+				if build_id == branch_list[branch].variants[variant].build_id:
+					if status != branch_list[branch].variants[variant].status:
+						branch_list[branch].set_result(variant, status, url, build_id)
+						save_to_file = True
+				elif build_id > branch_list[branch].variants[variant].build_id:
+					branch_list[branch].set_result(variant, status, url, build_id)
+					save_to_file = True
+
+	if save_to_file:
+		# Save results to file
+		with open(BRANCHES_STATUS, "w") as f:
+			f.write (PyJSONSerialization.dump(branch_list))
+
+except ValueError as exception:
+	# no data, this is not a Gitlab request
+	pass
+except Exception as exception:
+	pass
+
+
+# ###############################################################
+# Display CI results
+#
+# Build a table showing :
+# - Branch name
+# - status of all the build variants of this branch
+
+print '''Content-type: text/html; charset=utf-8'
 
 <html>
 <head>
-  <title>Etat des branches</title>
-  <meta name="description" content="Etat des branches de l'intégration continue" />
-  <meta name="keywords"    content="Jenkins, Navineo, Embarqué" />
-  <style>''' + CSS + '''
-  </style>
+  <title>Branch status</title>
+  <meta http-equiv="refresh" content="5">
+  <style>''' + CSS + '''</style>
 </head>
 <script language="javascript" type="text/javascript">
 function ShowMenu(self, e) {
 console.log(self.id)
-  var posx = e.clientX + window.pageXOffset +'px'; // Left Position of Mouse Pointer
+  var posx = e.clientX + window.pageXOffset + 'px'; // Left Position of Mouse Pointer
   var posy = e.clientY + window.pageYOffset + 'px'; // Top Position of Mouse Pointer
   var menu = self.querySelectorAll("#ctx_menu")[0];
   menu.style.visibility = 'visible';
@@ -215,11 +301,6 @@ console.log(self.id)
 </script>
 '''
 
-# ###############################################################
-# Build a table showing :
-# - Branch name
-# - status of all the variant of this branch
-
 # Iterate through branch list to extract the list of variants
 variant_list = []
 for branch_name, branch_status in branch_list.iteritems():
@@ -232,33 +313,38 @@ print '''<div id="titre">Branch Status</div>'''
 print '''<table>'''
 print '''<tr><th/>'''
 for variant in variant_list:
-	print '''<th class="titre">''' + variant + '''</td>'''
+	variant_title = variant.split(":", 1)
+	print '''<th class="titre">''' + variant_title[0] + "<br/>" + variant_title[-1] + '''</td>''' # extract "quick" and "linux" from "quick:linux"
 print '''</tr>'''
 
 # Iterate through branches sorted by date of the last update
 cpt = 0
 for (branch_name, branch_status) in sorted(branch_list.items(), key=lambda(k,v): v.date_maj, reverse=True):
-	# Remove "origin/" (it's not the interesting part)
-	branch_name_short = branch_name.replace("origin/", "")
-	print '''<tr><td class="branche">''' + branch_name_short + '''</td>'''
-	
+	if branch_status.url:
+		print '''<tr><td class="branch"><a class="pipeline" href="'''+branch_status.url+'''">''' + branch_name + '''</a></td>'''
+	else:
+		print '''<tr><td class="branch">''' + branch_name + '''</a></td>'''
+
 	# Add a column for each variant
 	for variant in variant_list:
 		try:
-			status = branch_status.variants[variant].status
-			url    = branch_status.variants[variant].url
+			status   = branch_status.variants[variant].status
+			previous = branch_status.variants[variant].previous
+			if not previous:
+				previous = status
+			url      = branch_status.variants[variant].url
 			if url:
 				href = ''' href="''' + url + '''"'''
 			else:
 				href = ""
-			print '''<td class="''' + status + '''" onContextMenu="return ShowMenu(this, event);">
+			print '''<td class="''' + previous + '''" onContextMenu="return ShowMenu(this, event);">
 			<div id="ctx_menu">
-			<a href="?branch=''' + escape(branch_name) + '''&variant=''' + escape(variant) + '''&force_status=OK"/>Forcer OK</a>
+			<a href="?branch=''' + escape(branch_name) + '''&variant=''' + escape(variant) + '''&force_status=OK"/>Force OK</a>
 			</div>
-			<a class="''' + status + '''"''' + href + '''>''' + status + '''</a>
+			<a class="''' + status + '''"''' + href + '''>''' + status.upper() + '''</a>
 			</td>'''
 		except KeyError:
-			# The variant doen't exists for this branch
+			# The variant doesn't exists for this branch
 			print '''<td/>'''
 
 	print '''</tr>'''
@@ -268,4 +354,5 @@ for (branch_name, branch_status) in sorted(branch_list.items(), key=lambda(k,v):
 		break
 	
 print '''</table>
+'''+SCRIPT_VERSION+'''
 </html>'''
